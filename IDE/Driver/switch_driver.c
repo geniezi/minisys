7c46999b0e9349e9380d1e3c2fa6dc3469ed2ba2
mh_chen <mh_chen@seu.edu.cn>
2024-01-13 16:58:01 +0800

    Delete IDE directory

--- IDE/Driver/switch_driver.c
+++
@@ -1,6 +0,0 @@
-int read_switch() {
-    int a;
-    // a = $0xFFFFFC70;
-    a=load_mem(0xFFFFFC70);
-    return a;
-}
--- IDE/util/util.c
+++
@@ -1,8 +0,0 @@
-// delay 0.5 * halfsec secs
-void delay(int halfsec) {
-  halfsec = halfsec * 250000;
-  while (halfsec > 0) {
-    halfsec = halfsec - 1;
-  }
-  return;
-}
